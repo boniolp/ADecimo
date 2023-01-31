4f297ee53f4f8d16de66fe776902ee6f7d9711da
Paul Boniol <paul.boniol@free.fr>
2023-02-01 00:24:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def plot_box_plot(df):
     fig = plt.figure(figsize=(10, 30))
     order = list(df_toplot.median().sort_values().index)[::-1]
     sns.boxplot(data=df_toplot,order=order,showfliers = False, orient='h')  
-    sns.stripplot(data=df_toplot,order=order,size=4, color=".3", linewidth=0,orient='h')
+    sns.stripplot(data=df_toplot,order=order,size=1, color=".3", linewidth=0,orient='h')
     st.pyplot(fig)
     
 
