bf960bfc25a13bb26617cdf22ce48f7609d0a25b
Paul Boniol <paul.boniol@free.fr>
2023-01-31 17:14:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,16 @@ import streamlit as st
 with st.sidebar:
     st.markdown('# ADecimo') 
     st.markdown('### Model selection for time series anomaly detection') 
+    metric_name = st.selectbox('Pick an accuracy measure', ['AUC-ROC', 'AUC-PR', 'VUS-ROC', 'VUS-PR'])
     
-tab_acc, tab_time = st.tabs(["Accuracy", "Execution Time"])  
+tab_acc, tab_time, tab_stats = st.tabs(["Accuracy", "Execution Time", "Datasets"])  
 with tab_acc:
     st.markdown('# Accuracy Evaluation')
+    st.markdown(metric_name)
     
 with tab_time:
     st.markdown('# Execution Time Evaluation')
     
-
+with tab_stats:
+    st.markdown('# Dataset Statistics')
+    
