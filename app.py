66d2fb12ba53676c660d110d97175346f1a3a096
Paul Boniol <paul.boniol@free.fr>
2023-01-31 17:01:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,12 @@ import streamlit as st
 with st.sidebar:
     st.markdown('# ADecimo') 
     st.markdown('### Model selection for time series anomaly detection') 
+    
+    
+with tab_acc:
+    st.markdown('# Accuracy Evaluation')
+    
+with tab_time:
+    st.markdown('# Execution Time Evaluation')
+    
+
