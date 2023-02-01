e87f65e7606614f9cb3a9bc73aded17fc9419927
Paul Boniol <paul.boniol@free.fr>
2023-02-01 15:02:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -70,10 +70,9 @@ with tab_time:
 with tab_stats:
     st.markdown('# Dataset Statistics')
     st.dataframe(df[dataset_stats])
-    fig = plt.figure(figsize=(10,10))
     g = sns.PairGrid(df[dataset_stats_real], diag_sharey=False)
     g.map_upper(sns.scatterplot, s=15)
     g.map_lower(sns.kdeplot)
     g.map_diag(sns.kdeplot, lw=2)
-    st.pyplot(fig)
+    st.pyplot(g)
     
