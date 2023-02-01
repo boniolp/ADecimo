c2cc8a528c1590688218e23c6a48a06e329b4651
Paul Boniol <paul.boniol@free.fr>
2023-02-01 10:49:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -65,14 +65,14 @@ tab_acc, tab_time, tab_stats = st.tabs(["Accuracy", "Execution Time", "Datasets"
 with tab_acc:
     st.markdown('# Accuracy Evaluation')
     st.markdown('Overall evaluation of 125 classification algorithm used for model selection for anoamly detection. We use the 496 randomly selected time series from the TSB-UAD benchmark. Measure used: {}'.format(metric_name))
-    generate_dataframe(df,datasets,methods_family,length,type_exp='_score')
+    df_toplot = generate_dataframe(df,datasets,methods_family,length,type_exp='_score')
     st.dataframe(df_toplot)
     plot_box_plot(df_toplot)
     
 with tab_time:
     st.markdown('# Execution Time Evaluation')
     st.markdown('Overall evaluation of 125 classification algorithm used for model selection for anoamly detection. We use the 496 randomly selected time series from the TSB-UAD benchmark. Measure used: Prediction time in seconds')
-    generate_dataframe(df,datasets,methods_family,length,type_exp='_inf')
+    df_toplot = generate_dataframe(df,datasets,methods_family,length,type_exp='_inf')
     st.dataframe(df_toplot)
     plot_box_plot(df_toplot)
     
