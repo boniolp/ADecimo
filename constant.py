fe392477034d8e7d36f4ce206cf832789e7fc31b
Paul Boniol <paul.boniol@free.fr>
2023-02-25 10:22:54 +0100

    Update constant.py

--- constant.py
+++ constant.py
@@ -109,4 +109,30 @@ We propose a benchmark and an evaluation of 16 time series classifiers used as m
 """
 
 
+text_description_dataset = f"""
+We use the time series in the TSB-UAD benchmark (16 public datasets from heterogeneous domains).
+Briefly, TSB-UAD includes the following datasets:
+
+| Dataset    | Description|
+|:--|:---------:|
+|Dodgers| is a loop sensor data for the Glendale on-ramp for the 101 North freeway in Los Angeles, and the anomalies represent unusual traffic after a Dodgers game.|
+|ECG| is a standard electrocardiogram dataset, and the anomalies represent ventricular premature contractions. We split one long series (MBA_ECG14046) with length ∼ 1e7) into 47 series by first identifying the periodicity of the signal.|
+|IOPS| is a dataset with performance indicators that reflect the scale, quality of web services, and health status of a machine.|
+|KDD21| is a composite dataset released in a recent SIGKDD 2021 competition with 250 time series.|
+|MGAB| is composed of Mackey-Glass time series with non-trivial anomalies. Mackey-Glass time series exhibit chaotic behavior that is difficult for the human eye to distinguish.|
+|NAB| is composed of labeled real-world and artificial time series including AWS server metrics, online advertisement clicking rates, real time traffic data, and a collection of Twitter mentions of large publicly-traded companies.|
+|SensorScope| is a collection of environmental data, such as temperature, humidity, and solar radiation, collected from a typical tiered sensor measurement system.|
+|YAHOO| is a dataset published by Yahoo labs consisting of real and synthetic time series based on the real production traffic to some of the Yahoo production systems.|
+|Daphnet| contains the annotated readings of 3 acceleration sensors at the hip and leg of Parkinson’s disease patients that experience freezing of gait (FoG) during walking tasks.|
+|GHL| is a Gasoil Heating Loop Dataset and contains the status of 3 reservoirs, such as the temperature and level. Anomalies indicate changes in max temperature or pump frequency.|
+|Genesis| is a portable pick-and-place demonstrator which uses an air tank to supply all the gripping and storage units.|
+|MITDB| contains 48 half-hour excerpts of two-channel ambulatory ECG recordings obtained from 47 subjects studied by the BIH Arrhythmia Laboratory between 1975 and 1979.|
+|OPPORTUNITY (OPP)| is a dataset devised to benchmark human activity recognition algorithms (e.g., classification, automatic data segmentation, sensor fusion, and feature extraction). The dataset comprises the readings of motion sensors recorded while users executed typical daily activities.|
+|Occupancy| contains experimental data used for binary classification (room occupancy) from temperature, humidity, light, and CO2. Ground-truth occupancy was obtained from time-stamped pictures that were taken every minute.|
+|SMD (Server Machine Dataset)| is a 5-week-long dataset collected from a large Internet company. This dataset contains 3 groups of entities from 28 different machines.|
+|SVDB| includes 78 half-hour ECG recordings chosen to supplement the examples of supraventricular arrhythmias in the MIT-BIH Arrhythmia Database.|
+
+
+"""
+
 
