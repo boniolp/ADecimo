b7fb27a10f23d198b03b2f9758db661dc0525268
Paul Boniol <paul.boniol@free.fr>
2023-03-24 18:07:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -112,8 +112,9 @@ with tab_acc:
 					ts_data = ts_data_raw[:,0].astype(float)
 					ts_data = ts_data[:min(len(ts_data),40000)]
 					
-					pred_detector = run_model(ts_data)
+					pred_detector,shape_seq = run_model(ts_data)
 					st.markdown(pred_detector)
+					st.markdown(shape_seq)
 					
 					
 					trace_scores_uplaod.append(go.Scattergl(
