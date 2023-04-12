ae0476bd0c8eaac1f10fb509995aacffad11e2eb
Paul Boniol <paul.boniol@free.fr>
2023-04-12 18:01:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -112,9 +112,9 @@ with tab_acc:
 					ts_data = ts_data_raw[:,0].astype(float)
 					ts_data = ts_data[:min(len(ts_data),40000)]
 					
-					#pred_detector,shape_seq = run_model(ts_data)
-					#st.markdown("The Detector to select is {}".format(pred_detector))
-					#st.markdown(shape_seq)
+					pred_detector,shape_seq = run_model(ts_data)
+					st.markdown("The Detector to select is {}".format(pred_detector))
+					
 					
 					
 					trace_scores_uplaod.append(go.Scattergl(
